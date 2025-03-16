504394f84f96073194b6b69e421196d932dcb58d
Konsta Poikolainen <ag1539@student.jamk.fi>
2025-03-16 15:05:11 +0200

    Päivitetty otsikko netlify-versioon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <main>
-	<h1>Konsta Poikolainen ALE-laskuri</h1>
+	<h1>Konsta Poikolainen ALE-laskuri Netlifyssä</h1>
 
 	<label for="hinta">Hinta</label>
 	<input id="hinta" type="number" min="1" max={maxHinta} oninput={hintaTsekki} />
