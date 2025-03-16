e5841260fab6324a3b13e639fd731f3582aa6903
Konsta Poikolainen <ag1539@student.jamk.fi>
2025-03-16 14:56:11 +0200

    Lisätty netlify konfiguraatio

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter() // Vaihdettu adapter-auto -> adapter-vercel
-	}
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
